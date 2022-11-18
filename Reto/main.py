db8f82a1834504fe17b561fa6f35eef9222d2b0a
Jorge Del Barco <jdelbarcogarza@gmail.com>
2022-11-18 12:36:08 -0600

    ahora el modelo no toma ningun argumento en su construccion

---
+++ Reto/main.py
@@ -0,0 +1,10 @@
+
+from Reto.StreetModel import StreetModel
+
+def modelo_de_reto():
+
+    # 10 autos en el modelo. * Valor NO modificable
+    model = StreetModel()
+
+    print('num of ag', model.num_agents)
+
