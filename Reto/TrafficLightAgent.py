49d648556311e8b9cdf3e7ae8776531f6f455ba6
Jorge Del Barco <jdelbarcogarza@gmail.com>
2022-11-18 12:31:36 -0600

    la clase de semaforo se convirtio en un agnete

--- Reto/TrafficLightAgent.py
+++ Reto/TrafficLightAgent.py
@@ -1,26 +1,18 @@
 import mesa
 
 # clase de semaforo para controlar flujo de tráfico
-class TrafficLight(mesa.Agent):
+class TrafficLightAgent(mesa.Agent):
 
-    def __init__(self, unique_id, model, is_green, position_x, position_y):
+    def __init__(self, unique_id, model):
 
-        super().__init__(unique_id, model, is_green, position_x, position_y)
+        super().__init__(unique_id, model)
 
-        self.name_id = 'TF'  # id para identificar en la matriz de espacio
-        self.is_green = is_green  # boolean
-
-        # row
-        self.position_x = position_x  # int
-
-        # column
-        self.position_y = position_y  # int
+        self.name_id = 'T'  # id para identificar en la matriz de espacio
+        self.is_green = True  # boolean
 
     def get_position(self):
-        return [self.position_x, self.position_y]
 
-    def get_is_green(self):
-        return self.is_green
+        return self.pos
 
     def change_light(self):
         self.is_green = not self.is_green
