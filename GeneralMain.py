ba9a0aa3fd04ffd306c15caa48dd6ffc6426f074
Jorge Del Barco <jdelbarcogarza@gmail.com>
2022-11-18 12:31:52 -0600

    archivo que corre el modelo

--- GeneralMain.py
+++ GeneralMain.py
@@ -1,8 +1,10 @@
 
 from ejemplo2_add_space.main import basic_example_space
 from M1_Actividad.main import cleaner_robot_activity
+from Reto.main import modelo_de_reto
 
 if __name__ == "__main__":
     # basic_example()
     # basic_example_space()
-    cleaner_robot_activity()
+    # cleaner_robot_activity()
+    modelo_de_reto()
