23712e192d69acdde0fae585bf57546ab12a8357
Jorge Del Barco <jdelbarcogarza@gmail.com>
2022-11-19 20:09:50 -0600

    carros ya se mueven correctamente con semaforo en rojo

--- Reto/CarAgent.py
+++ Reto/CarAgent.py
@@ -12,18 +12,16 @@ class CarAgent(mesa.Agent):
 
         # coordenadas actuales del agente
         row, col = self.pos
+        print('analizando agente en', self.pos)
 
         # mover los agentes de flujo continuo en carril 0
         if row == 0:
-
-            # mover en matriz impresa
 
             # poner simbolo de calle
             self.model.space_matrix[row][col] = '_'
 
             # mover agente 1 casilla a la derecha en grid de modelo
             self.model.grid.move_agent(self, (0, self.pos[1] + 1))
-            print('soy agente: ', self.unique_id, 'ando en ', self.pos)
 
             row, col = self.pos
             # colocar simbolo de Carro en matriz impresa
@@ -33,24 +31,75 @@ class CarAgent(mesa.Agent):
         elif row == 1:
 
             # checar si hay carro adelante para moverse
-            surroundings = self.mode.grid.get_neighborhood(
+            surroundings = self.model.grid.get_neighborhood(
                 self.pos,
                 moore=True,
                 include_center=False
             )
 
-            print('alrededor', surroundings)
+            print(surroundings)
 
-            # checar si esta en la casilla de semaforo
+            # CASO LIMITE EN ULTIMA COLUMNA 8
+            if col == 8:
 
+                # Si casilla esta libre, mover al agente
+                if type(self.model.grid[surroundings[3]]) != CarAgent:
 
+                    # modificar matriz impresa
+                    self.model.space_matrix[row][col] = '_'
 
-            pass
+                    # mover agente
+                    self.model.grid.move_agent(self, (1, col + 1))
+
+                    row, col = self.pos
+                    # colocar simbolo de Carro en matriz impresa
+                    self.model.space_matrix[row][col] = 'C'
+
+                # si hay un carro parado adelante, no nos podremos mover
+                elif type(self.model.grid[surroundings[3]]) == CarAgent:
+                    print('no me muevo. Tengo coche adelante PARADO. Soy:', self.unique_id, self.pos)
+
+                    pass
+
+            # checar si agente esta donde debe pararse con el semaforo de main street
+            elif self.pos == (1, 3):
+
+                # si el semaforo main st. esta en verde, avanzan los carros
+                if self.model.grid[2][3].is_green:
+
+                    # modificar matriz impresa
+                    self.model.space_matrix[row][col] = '_'
+
+                    # mover agente
+                    self.model.grid.move_agent(self, (1, col + 1))
+
+                    row, col = self.pos
+                    # colocar simbolo de Carro en matriz impresa
+                    self.model.space_matrix[row][col] = 'C'
+
+                    # si el semaforo esta en rojo
+                else:
+                    print('semaforo main st. en rojo no me muevo', self.unique_id, self.pos)
+
+            # los autos se mueven normal hacia la derecha
+            else:
+
+                # modificar matriz impresa
+                self.model.space_matrix[row][col] = '_'
+
+                # mover agente
+                self.model.grid.move_agent(self, (1, col + 1))
+
+                row, col = self.pos
+                # colocar simbolo de Carro en matriz impresa
+                self.model.space_matrix[row][col] = 'C'
+
 
     def get_agent_pos(self):
 
         # retorna una tupla
         return self.pos
+
 
     def step(self):
         self.move()
