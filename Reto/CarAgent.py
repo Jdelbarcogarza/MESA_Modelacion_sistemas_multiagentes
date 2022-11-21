48a3236a4dcc6255000688447b670d37a295fcda
Jorge Del Barco <jdelbarcogarza@gmail.com>
2022-11-21 16:32:23 -0600

    se retiraron prints de debuggueo

--- Reto/CarAgent.py
+++ Reto/CarAgent.py
@@ -24,7 +24,7 @@ class CarAgent(mesa.Agent):
 
         # coordenadas actuales del agente
         row, col = self.pos
-        print('analizando agente en', self.pos)
+        # print('analizando agente en', self.pos) # DEBUG PRINT
 
         # obtener alrededores del agente
         surroundings = self.model.grid.get_neighborhood(
@@ -41,6 +41,8 @@ class CarAgent(mesa.Agent):
         # agentes de carril 1 (Main st.)
         elif row == 1:
 
+            # print(surroundings) # DEBUG PRINT
+
             # CASO LIMITE EN ULTIMA COLUMNA 8 o EN EL INICIO COLUMNA 0
             if col == 8 or col == 0:
 
@@ -53,7 +55,9 @@ class CarAgent(mesa.Agent):
 
                     # si hay un carro parado adelante, no nos podremos mover
                     elif type(self.model.grid[surroundings[3]]) == CarAgent:
-                        print('no me muevo. Tengo coche adelante PARADO. Soy:', self.unique_id, self.pos)
+                        # DEBUG PRINT
+                        # print('no me muevo. Tengo coche adelante PARADO. Soy:', self.unique_id, self.pos)
+                        pass
 
                 elif col == 8 and self.sub_st_car:
 
@@ -67,12 +71,13 @@ class CarAgent(mesa.Agent):
                     if self.model.grid.is_cell_empty((8, 4)):
                         self.model.grid.move_agent(self, (8, 4))
                     else:
-                        print('entrada a sub st. ocuapada', self.pos, 'soy', self.unique_id)
-
+                        # print('entrada a sub st. ocuapada', self.pos, 'soy', self.unique_id)  # DEBUG PRINT
+                        pass
 
             # checar si tienes un carro enfrente en medio de la matriz
             elif type(self.model.grid[surroundings[4]]) == CarAgent:
-                print('carro parado enfrente. No me muevo', self.unique_id, self.pos)
+                # print('carro parado enfrente. No me muevo', self.unique_id, self.pos) # DEBUG PRINT
+                pass
 
             # checar si agente esta donde debe pararse con el semaforo de main street
             elif self.pos == (1, 3):
@@ -84,8 +89,8 @@ class CarAgent(mesa.Agent):
 
                     # si el semaforo esta en rojo
                 else:
-                    print('semaforo main st. en rojo no me muevo', self.unique_id, self.pos)
-
+                    # print('semaforo main st. en rojo no me muevo', self.unique_id, self.pos)  # DEBUG PRINT
+                    pass
             # los autos se mueven normal hacia la derecha
             else:
 
@@ -94,12 +99,13 @@ class CarAgent(mesa.Agent):
         # movimiento de agente en calle secundaria (Sub st.)
         elif row >= 2:
 
-            print(surroundings)
+            # print(surroundings) # DEBUG PRINT
 
             # verificar si hay auto en frente
             if type(self.model.grid[surroundings[1]]) == CarAgent:
 
-                print('no avanzo, tengo carro enfrente', self.pos, 'soy', self.unique_id)
+                # print('no avanzo, tengo carro enfrente', self.pos, 'soy', self.unique_id)  # DEBUG PRINT
+                pass
 
             # si estas en casilla para semaforo
             elif self.pos == (2, 4):
@@ -119,7 +125,9 @@ class CarAgent(mesa.Agent):
                 # si semaforo esta en rojo
                 else:
 
-                    print('semaforo en sub st. en ROJO. No me puedo mover', self.pos, 'soy', self.unique_id)
+                    # DEBUG PRINT
+                    # print('semaforo en sub st. en ROJO. No me puedo mover', self.pos, 'soy', self.unique_id)
+                    pass
 
             # mover al agente por sub st.
             else:
