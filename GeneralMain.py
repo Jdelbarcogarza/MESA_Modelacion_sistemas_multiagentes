06cfc07928c57d5c94a5e88620939f34057b1fe3
Jorge Del Barco <jdelbarcogarza@gmail.com>
2022-11-29 07:32:10 -0600

    se corre modelo de evidencia 1

--- GeneralMain.py
+++ GeneralMain.py
@@ -4,11 +4,13 @@ from M1_Actividad.main import cleaner_robot_activity
 from Reto.main import modelo_de_reto
 from Evidencia_1.main import modelo_cajas
 
-#if __name__ == "__main__":
+if __name__ == "__main__":
     # basic_example()
     # basic_example_space()
     # cleaner_robot_activity()
 
     # ------ RETO ----------
     #modelo_de_reto()
-    # modelo_cajas()
+    modelo_cajas()
+
+
