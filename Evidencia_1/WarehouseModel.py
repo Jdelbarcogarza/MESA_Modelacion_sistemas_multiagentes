5c86c903e1f2bcaa7767f6bd27e5014c14f2565a
Jorge Del Barco <jdelbarcogarza@gmail.com>
2022-11-29 07:33:06 -0600

    se agregó matriz de espacio

--- Evidencia_1/WarehouseModel.py
+++ Evidencia_1/WarehouseModel.py
@@ -8,15 +8,32 @@ class BoxModel(mesa.Model):
         self.num_agents = N
         self.grid = mesa.space.MultiGrid(width, height, True)
         self.schedule = mesa.time.RandomActivation(self)
+
+        # Matriz para representar el espacio
+        self.space_matrix = [['_' for _ in range(width)] for _ in range(height)]
+
+
         # Create agents
         for i in range(self.num_agents):
             a = BoxAgent(i, self)
             self.schedule.add(a)
 
+            # colocar agentes de manera aleatoria en el grid.
             x = self.random.randrange(self.grid.width)
             y = self.random.randrange(self.grid.height)
-            self.grid.place_agent(a, (x,y))
+            self.grid.place_agent(a, (x, y))
+
+            # colocar en matriz de espacio agentes
+            self.space_matrix[x][y] = 'R'
+
+        self.print_space_matrix()
+
 
     def step(self):
         self.schedule.step()
 
+
+    def print_space_matrix(self):
+        # imprimir matriz
+        for row in self.space_matrix:
+            print(row)
