8ef57b4af82c8e38214bf1bef5772cbfb2d58bff
Jorge Del Barco <jdelbarcogarza@gmail.com>
2022-11-29 08:02:14 -0600

    ahora se agregan las cajas al espacio

--- Evidencia_1/WarehouseModel.py
+++ Evidencia_1/WarehouseModel.py
@@ -1,11 +1,12 @@
 import mesa
-from Evidencia_1.BoxRobotAgent import BoxAgent
+from Evidencia_1.RobotAgent import RobotAgent
+from Evidencia_1.BoxAgent import BoxAgent
 
 
 class BoxModel(mesa.Model):
 
-    def __init__(self, N, width, height):
-        self.num_agents = N
+    def __init__(self, N, num_boxes, width, height):
+        self.num_agents = N + num_boxes
         self.grid = mesa.space.MultiGrid(width, height, True)
         self.schedule = mesa.time.RandomActivation(self)
 
@@ -13,18 +14,38 @@ class BoxModel(mesa.Model):
         self.space_matrix = [['_' for _ in range(width)] for _ in range(height)]
 
 
-        # Create agents
-        for i in range(self.num_agents):
-            a = BoxAgent(i, self)
+        # Crear agentes robot
+        for i in range(N):
+            a = RobotAgent(i, self)
             self.schedule.add(a)
 
             # colocar agentes de manera aleatoria en el grid.
-            x = self.random.randrange(self.grid.width)
+            x = self.random.randrange(1, self.grid.width - 1)
             y = self.random.randrange(self.grid.height)
             self.grid.place_agent(a, (x, y))
 
             # colocar en matriz de espacio agentes
             self.space_matrix[x][y] = 'R'
+
+        for i in range(N, N + num_boxes):
+            a = BoxAgent(i, self)
+            self.schedule.add(a)
+
+            x = self.random.randrange(self.grid.width)
+            y = self.random.randrange(self.grid.height)
+
+            # verificar que no se coloque la caja sobre un robot en la inicialización del modelo
+            while not self.grid.is_cell_empty((x, y)):
+
+                x = self.random.randrange(self.grid.width)
+                y = self.random.randrange(self.grid.height)
+
+            # se coloca el agente en una celda vacía
+            self.grid.place_agent(a, (x, y))
+
+            self.space_matrix[x][y] = 'B'
+
+        # crear agentes de caja
 
         self.print_space_matrix()
 
